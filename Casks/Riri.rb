edff5ba8b3da172adf9af0fef1640f5fcdb4e4fd
LiuliFox <liulifox@qq.com>
2025-05-02 11:49:14 +0800

    chore: update

--- Casks/Riri.rb
+++ Casks/Riri.rb
@@ -1,8 +1,8 @@
 cask "riri" do
-  version "2.0.0"
+  version "2.0.3"
   arch arm: "ARM64", intel: "X64"
-  sha256 arm:   "cafe18e053cd19c04ec69c18c0391aaf99397dfb84df284e67f8e084f58ded2a",
-         intel: "527eba74dcd48d326515e265830eb244bc9f0d4bf65cc9acdcf13e94dd6f823f"
+  sha256 arm:   "1a037ed27cc5c181c1d05673c5932b74db1d7556b8526a5890b9e5da87c9fa9a",
+         intel: "b24c200a3262d9c9d48c3b04cf6b85997b5e900670bcf63350b9369489a0ebff"
 
   url "https://github.com/liulifox233/Riri/releases/download/v#{version}/Riri-macOS-#{arch}.zip",
       verified: "github.com/liulifox233/Riri"
